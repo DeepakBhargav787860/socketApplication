d20e2206b4cb032eeaa91df77ce2e0385d7f7dbd
DeepakBhargav787860 <Deepak.Bhargav787860@gmail.com>
2025-07-09 09:44:40 +0530

    new deploy

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-## Core Syrax User Interface
+##
 
-### Project Structure
-
-1. assets - all images, css files, font files, etc
-2. components (common) - common components, top of the mantine UI
-3. context - store management, top of the REDUX
-4. hooks(common) - custom hooks
-5. services (common) -  interfacing with any external API
-6. lib - For the various different libraries(Facade Pattern), eg axios
-7. layouts - For number of custom a sidebar, navbar,footer and other
-8. internal(important) - group by feature, its is contain (hooks,services,components and other).
-9. utils - utility functions
-10. data - contains global constant variables(JSON files ,theme information and others)
+1. chat application
--- src/main.tsx
+++ src/main.tsx
@@ -1,64 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {Provider} from 'react-redux'
-import store from './context/store'
-import '../src/assets/styles/mrq.css';
-import '../src/assets/styles/common.css';
-import App from './App'
-import {CookiesProvider} from "react-cookie";
-import disableDevtool from "disable-devtool";
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import store from "./context/store";
+import "../src/assets/styles/mrq.css";
+import "../src/assets/styles/common.css";
+import App from "./App";
+import { CookiesProvider } from "react-cookie";
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            refetchOnMount: false,
-            refetchOnReconnect: false,
-            retry: false,
-            staleTime: 0,
-        },
-
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 0,
     },
+  },
+});
 
-})
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </Provider>
+  </QueryClientProvider>
 
- const logout = () => async () => {
-
-
-     localStorage.setItem('Though of the day','सफलता भी उसी की तरफ़दार होती है मेहनतें जिसकी वफादार होती है।')
-        window.location.href = '/4o4😉😉😉😉😉😉😉😉😉😉4o4😉😉😉😉😉😉😉😉😉😉';
-
-
-}
-
-
-//Disable Browser Tool
-disableDevtool(
-    {url: import.meta.env.VITE_BASE_URL_V2,
-        disableSelect: false,
-        ondevtoolopen:logout(),
-        ignore: () => {return import.meta.env.DEV; //PROD
-    }});
-
-
-//Disable React Dev tool
-if (import.meta.env.MODE === 'production') {
-    disableReactDevTools(); //development or production
-}
-
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    // <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-            <CookiesProvider >
-                <App/>
-            </CookiesProvider>
-        </Provider>
-    </QueryClientProvider>
-
-    // </React.StrictMode>
-)
+  // </React.StrictMode>
+);
