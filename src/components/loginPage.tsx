fcfaeb423c5cf9f6fe2407b800e003305c9f8e97
DeepakBhargav787860 <Deepak.bhargav787860@gmail.com>
2025-08-27 22:26:53 +0530

    Update loginPage.tsx

--- src/components/loginPage.tsx
+++ src/components/loginPage.tsx
@@ -196,7 +196,7 @@ const LoginPage = () => {
                   fontWeight: 600,
                 }}
               >
-                Login Shell
+                Login 
               </Button>
               <Button
                 onClick={() => navigate("/signUp")}
